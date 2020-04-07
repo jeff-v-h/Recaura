025bf9fc39967874c2a47753e4ed9174aae63492
Jeffrey Huang <jeffvh@outlook.com>
2020-04-07 10:46:51 +1000

    add redux devtools extension

--- Dawn.ClientApp/src/store/configureStore.ts
+++ Dawn.ClientApp/src/store/configureStore.ts
@@ -1,5 +1,6 @@
-import { applyMiddleware, combineReducers, compose, createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
 import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 import { connectRouter, routerMiddleware } from "connected-react-router";
 import { History } from "history";
 import { ApplicationState, reducers } from "./";
@@ -8,23 +9,21 @@ export default function configureStore(
   history: History,
   initialState?: ApplicationState
 ) {
-  const middleware = [thunk, routerMiddleware(history)];
-
   const rootReducer = combineReducers({
     ...reducers,
-    router: connectRouter(history)
+    router: connectRouter(history),
   });
 
-  const enhancers = [];
+  const middlewares = [thunk, routerMiddleware(history)];
+  const middlewareEnhancer = applyMiddleware(...middlewares);
+
+  const enhancers = [middlewareEnhancer];
   const windowIfDefined =
     typeof window === "undefined" ? null : (window as any);
   if (windowIfDefined && windowIfDefined.__REDUX_DEVTOOLS_EXTENSION__) {
     enhancers.push(windowIfDefined.__REDUX_DEVTOOLS_EXTENSION__());
   }
+  const composedEnhancers = composeWithDevTools(...enhancers);
 
-  return createStore(
-    rootReducer,
-    initialState,
-    compose(applyMiddleware(...middleware), ...enhancers)
-  );
+  return createStore(rootReducer, initialState, composedEnhancers);
 }
