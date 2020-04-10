91f879f2ec09597055466ffd279b940c5e1d660a
Jeffrey Huang <jeffvh@outlook.com>
2020-04-10 10:19:28 +1000

    extract patient description into separate stateless component

--- Dawn.ClientApp/src/components/patient/Patient.tsx
+++ Dawn.ClientApp/src/components/patient/Patient.tsx
@@ -4,9 +4,7 @@ import { RouteComponentProps, withRouter } from "react-router-dom";
 import { compose } from "redux";
 import * as PatientStore from "../../store/Patient";
 import { ApplicationState } from "../../store";
-import { Descriptions } from "antd";
-
-const Item = Descriptions.Item;
+import PatientDescription from "./PatientDescription";
 
 type Props = PatientStore.PatientState &
   typeof PatientStore.actionCreators &
@@ -27,17 +25,9 @@ class Patient extends React.Component<Props> {
     if (!patient) return null;
 
     return (
-      <Descriptions title="User Info" bordered>
-        <Item label="Title">{patient.honorific}</Item>
-        <Item label="First Name">{patient.firstName}</Item>
-        <Item label="Last Name">{patient.firstName}</Item>
-        <Item label="DOB">{patient.firstName}</Item>
-        <Item label="Email">{patient.email}</Item>
-        <Item label="Home Ph">{patient.homePhone}</Item>
-        <Item label="Mobile Ph">{patient.mobilePhone}</Item>
-        <Item label="Gender">{patient.gender}</Item>
-        <Item label="Occupation">{patient.occupation}</Item>
-      </Descriptions>
+      <>
+        <PatientDescription patient={patient} />
+      </>
     );
   }
 
