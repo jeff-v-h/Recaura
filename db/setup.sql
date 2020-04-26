b3714889788931af9c31311c9dc9996c66f4d803
jeff-h <jeffvh@outlook.com>
2020-04-26 10:51:04 +1000

    ensure database and tables created

--- db/setup.sql
+++ db/setup.sql
@@ -14,7 +14,7 @@ CREATE TABLE Patients (
     MobilePhone NVARCHAR(40),
     Gender NVARCHAR(10),
     Occupation NVARCHAR(50)
-    CONSTRAINT PK_Patients PRIMARY KEY Id
+    CONSTRAINT PK_Patients PRIMARY KEY (Id)
 );
 GO
 CREATE TABLE Practitioners (
@@ -29,18 +29,17 @@ CREATE TABLE Practitioners (
     MobilePhone NVARCHAR(40),
     Gender NVARCHAR(10),
     JobLevel NVARCHAR(50),
-    RegistrationID NVARCHAR(50)
-    CONSTRAINT PK_Practitioners PRIMARY KEY Id
+    RegistrationID NVARCHAR(50),
+    CONSTRAINT PK_Practitioners PRIMARY KEY (Id)
 );
 GO
 CREATE TABLE CaseFiles (
     Id INT IDENTITY (1, 1),
     PatientId INT,
     [Name] NVARCHAR(MAX),
-    Created DATE
-    CONSTRAINT PK_CaseFiles PRIMARY KEY Id
-    CONSTRAINT FK_CaseFiles_Patients_PatientId FOREIGN KEY PatientId REFERENCES Patients(Id)
-    ON DELETE CASCADE
+    Created DATE,
+    CONSTRAINT PK_CaseFiles PRIMARY KEY (Id),
+    CONSTRAINT FK_CaseFiles_Patients_PatientId FOREIGN KEY (PatientId) REFERENCES Patients(Id) ON DELETE CASCADE
 );
 GO
 CREATE TABLE Consultations (
@@ -52,12 +51,10 @@ CREATE TABLE Consultations (
     SubjectiveId INT,
     ObjectiveId INT,
     Plans NVARCHAR(MAX),
-    Treatments NVARCHAR(MAX)
-    CONSTRAINT PK_Consultations PRIMARY KEY Id
-    CONSTRAINT FK_Consultations_CaseFiles_CaseFileId FOREIGN KEY CaseFileId REFERENCES CaseFiles(Id)
-    ON DELETE CASCADE
-    CONSTRAINT FK_Consultations_Practitioners_PractitionerId FOREIGN KEY PractitionerId REFERENCES Practitioners(Id)
-    ON DELETE CASCADE
+    Treatments NVARCHAR(MAX),
+    CONSTRAINT PK_Consultations PRIMARY KEY (Id),
+    CONSTRAINT FK_Consultations_CaseFiles_CaseFileId FOREIGN KEY (CaseFileId) REFERENCES CaseFiles(Id) ON DELETE CASCADE,
+    CONSTRAINT FK_Consultations_Practitioners_PractitionerId FOREIGN KEY (PractitionerId) REFERENCES Practitioners(Id) ON DELETE CASCADE
 );
 GO
 CREATE TABLE ObjectiveAssessments (
@@ -65,10 +62,9 @@ CREATE TABLE ObjectiveAssessments (
     ConsultationId INT,
     Observation NVARCHAR(MAX),
     Palpation NVARCHAR(MAX),
-    Additional NVARCHAR(MAX)
-    CONSTRAINT PK_ObjectiveAssessments PRIMARY KEY Id
-    CONSTRAINT FK_ObjectiveAssessments_Consultations_ConsultationId FOREIGN KEY ConsultationId REFERENCES Consultations(Id)
-    ON DELETE CASCADE
+    Additional NVARCHAR(MAX),
+    CONSTRAINT PK_ObjectiveAssessments PRIMARY KEY (Id),
+    CONSTRAINT FK_ObjectiveAssessments_Consultations_ConsultationId FOREIGN KEY (ConsultationId) REFERENCES Consultations(Id) ON DELETE CASCADE
 );
 GO
 CREATE TABLE SubjectiveAssessments (
@@ -84,8 +80,7 @@ CREATE TABLE SubjectiveAssessments (
     SocialHistory NVARCHAR(MAX),
     Imaging NVARCHAR(MAX),
     GeneralHealth NVARCHAR(MAX),
-    CONSTRAINT PK_SubjectiveAssessments PRIMARY KEY Id
-    CONSTRAINT FK_SubjectiveAssessments_Consultations_ConsultationId FOREIGN KEY ConsultationId REFERENCES Consultations(Id)
-    ON DELETE CASCADE
+    CONSTRAINT PK_SubjectiveAssessments PRIMARY KEY (Id),
+    CONSTRAINT FK_SubjectiveAssessments_Consultations_ConsultationId FOREIGN KEY (ConsultationId) REFERENCES Consultations(Id) ON DELETE CASCADE
 );
 GO
