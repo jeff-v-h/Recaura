d7e7f447767298d7222fed1dd316b75d436ed483
jeff-h <jeffvh@outlook.com>
2020-04-28 09:14:13 +1000

    move csv seed data into separate folder

--- db/import-data.sh
+++ db/import-data.sh
@@ -1,3 +1,4 @@
+# Currently open issue for a better way to import data: https://github.com/Microsoft/mssql-docker/issues/229
 # wait for the SQL Server to come up
 sleep 90s
 
@@ -5,10 +6,10 @@ sleep 90s
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "recApp123" -i setup.sql
 
 # import the data from the csv file
-/opt/mssql-tools/bin/bcp Recauradb.dbo.Patients in "/usr/work/patients.csv" -c -t'|' -S localhost -U sa -P "recApp123"
-/opt/mssql-tools/bin/bcp Recauradb.dbo.Practitioners in "/usr/work/practitioners.csv" -c -t'|' -S localhost -U sa -P "recApp123"
-/opt/mssql-tools/bin/bcp Recauradb.dbo.CaseFiles in "/usr/work/casefiles.bcp" -c -t',' -S localhost -U sa -P "recApp123"
-/opt/mssql-tools/bin/bcp Recauradb.dbo.CaseFiles in "/usr/work/casefiles2.bcp" -c -t',' -S localhost -U sa -P "recApp123"
-/opt/mssql-tools/bin/bcp Recauradb.dbo.Consultations in "/usr/work/consults.csv" -c -t'|' -S localhost -U sa -P "recApp123"
-/opt/mssql-tools/bin/bcp Recauradb.dbo.ObjectiveAssessments in "/usr/work/objective.csv" -c -t'|' -S localhost -U sa -P "recApp123"
-/opt/mssql-tools/bin/bcp Recauradb.dbo.SubjectiveAssessments in "/usr/work/subjective.csv" -c -t'|' -S localhost -U sa -P "recApp123"
+/opt/mssql-tools/bin/bcp Recauradb.dbo.Patients in "/usr/work/seeddata/patients.csv" -c -t'|' -S localhost -U sa -P "recApp123"
+/opt/mssql-tools/bin/bcp Recauradb.dbo.Practitioners in "/usr/work/seeddata/practitioners.csv" -c -t'|' -S localhost -U sa -P "recApp123"
+/opt/mssql-tools/bin/bcp Recauradb.dbo.CaseFiles in "/usr/work/seeddata/casefiles.bcp" -c -t',' -S localhost -U sa -P "recApp123"
+/opt/mssql-tools/bin/bcp Recauradb.dbo.CaseFiles in "/usr/work/seeddata/casefiles2.bcp" -c -t',' -S localhost -U sa -P "recApp123"
+/opt/mssql-tools/bin/bcp Recauradb.dbo.Consultations in "/usr/work/seeddata/consults.csv" -c -t'|' -S localhost -U sa -P "recApp123"
+/opt/mssql-tools/bin/bcp Recauradb.dbo.ObjectiveAssessments in "/usr/work/seeddata/objective.csv" -c -t'|' -S localhost -U sa -P "recApp123"
+/opt/mssql-tools/bin/bcp Recauradb.dbo.SubjectiveAssessments in "/usr/work/seeddata/subjective.csv" -c -t'|' -S localhost -U sa -P "recApp123"
