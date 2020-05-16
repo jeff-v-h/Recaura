3a75249f603e26b9c5072a9a4a1c283ad7fce48c
jeff-h <jeffvh@outlook.com>
2020-05-16 18:59:32 +1000

    remove old unused db folder

--- db/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-# SQL Server process must be right most command to be the foreground process.
-# All other commands are run in the background. 
-# If script was in foreground, the container would shutdown when it completes
-/usr/work/import-data.sh & /opt/mssql/bin/sqlservr
