30458e9aae90549ade9b6dff7acfe669c9140511
jeff-h <jeffvh@outlook.com>
2020-05-20 14:18:13 +1000

    move casefilepage to casefile folder

--- client/src/Routes.tsx
+++ client/src/Routes.tsx
@@ -4,7 +4,7 @@ import Dashboard from './components/dashboard/Dashboard';
 import PatientPage from './components/patient/PatientPage';
 import CasefilesPage from './components/casefiles/CasefilesPage';
 import ConsultationPage from './components/consultation/ConsultationPage';
-import CasefilePage from './components/file/CasefilePage';
+import CasefilePage from './components/casefile/CasefilePage';
 import PatientsPage from './components/patients/PatientsPage';
 import NewPatientPage from './components/patient/NewPatientPage';
 import NewCasefilePage from './components/casefile/NewCasefilePage';
