0f1ded5106d9ce113091d7fe5a51be3bbe804014
jeff-h <jeffvh@outlook.com>
2020-05-23 20:42:10 +1000

    add consultation router tests

--- api/src/routers/consultationRouter.js
+++ api/src/routers/consultationRouter.js
@@ -78,7 +78,7 @@ router.patch('/consultations/:id', async (req, res) => {
   const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
 
   if (!isValidOperation) {
-    return res.status(404).send({ error: 'At least one property in object is invalid for updating!' });
+    return res.status(400).send({ error: 'At least one property in object is invalid for updating!' });
   }
 
   try {
