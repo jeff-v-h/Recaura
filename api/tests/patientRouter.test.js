e4830c5fc1252182c1bcec7fbd7eee08eccde972
jeff-h <jeffvh@outlook.com>
2020-05-23 17:32:12 +1000

    create tests for CRUD endpoints in patient router

--- api/tests/patientRouter.test.js
+++ api/tests/patientRouter.test.js
@@ -22,7 +22,7 @@ afterAll(async () => {
   await mongoServer.stop();
 });
 
-describe('Patient Endpoints', () => {
+describe('Patient Router', () => {
   const patient = {
     honorific: 'Miss',
     firstName: 'Jane',
@@ -40,16 +40,92 @@ describe('Patient Endpoints', () => {
     jest.restoreAllMocks();
   });
 
-  it('should return 201', async () => {
-    const res = await request(app).post('/patients').send(patient);
+  describe('post endpoint', () => {
+    it('should return 201', async () => {
+      const res = await request(app).post('/patients').send(patient);
 
-    expect(res.statusCode).toBe(201);
+      expect(res.statusCode).toBe(201);
+    });
+
+    it('should return the correct data', async () => {
+      // jest.spyOn(Patient.prototype, 'save').mockImplementationOnce(() => Promise.resolve(patient));
+      const res = await request(app).post('/patients').send(patient);
+
+      expect(res.body).toEqual({ ...patient, id: res.body.id, updatedAt: res.body.updatedAt });
+    });
+
+    it('should return 400 when not valid', async () => {
+      const res = await request(app).post('/patients').send({});
+
+      expect(res.statusCode).toBe(400);
+    });
   });
 
-  it('should return the correct data', async () => {
-    // jest.spyOn(Patient.prototype, 'save').mockImplementationOnce(() => Promise.resolve(patient));
-    const res = await request(app).post('/patients').send(patient);
+  describe('get endpoint', () => {
+    it('should return 200', async () => {
+      const res = await request(app).get('/patients').send(patient);
 
-    expect(res.body).toEqual({ ...patient, id: res.body.id, updatedAt: res.body.updatedAt });
+      expect(res.statusCode).toBe(200);
+    });
+
+    it('should return the correct data', async () => {
+      const newPatient = new Patient(patient);
+      await newPatient.save();
+
+      const res = await request(app).get(`/patients/${newPatient.id}`);
+      const { id, updatedAt } = res.body;
+
+      expect(res.body).toEqual({ ...patient, id, updatedAt });
+    });
+
+    it('should return 404 if not found', async () => {
+      const res = await request(app).get(`/patients/5eba8469b88f212f1012c357`);
+
+      expect(res.statusCode).toBe(404);
+    });
+  });
+
+  describe('patch endpoint', () => {
+    it('should return 200', async () => {
+      const savedPatient = new Patient(patient);
+      await savedPatient.save();
+      const patchedData = { firstName: 'new name' };
+      const res = await request(app).patch(`/patients/${savedPatient.id}`).send(patchedData);
+
+      expect(res.statusCode).toBe(200);
+    });
+
+    it('should return the correct data', async () => {
+      const savedPatient = new Patient(patient);
+      await savedPatient.save();
+      const patchedData = { firstName: 'new name' };
+
+      const res = await request(app).patch(`/patients/${savedPatient.id}`).send(patchedData);
+      const { id, updatedAt } = res.body;
+
+      expect(res.body).toEqual({ ...patient, id, updatedAt, firstName: patchedData.firstName });
+    });
+
+    it('should return 400 if invalid property', async () => {
+      const res = await request(app).patch(`/patients/1`).send({ fakeProp: 'test' });
+
+      expect(res.statusCode).toBe(400);
+    });
+  });
+
+  describe('delete endpoint', () => {
+    it('should return 200', async () => {
+      const savedPatient = new Patient(patient);
+      await savedPatient.save();
+      const res = await request(app).delete(`/patients/${savedPatient.id}`);
+
+      expect(res.statusCode).toBe(200);
+    });
+
+    it('should return 404 if no existing patient', async () => {
+      const res = await request(app).patch(`/patients/5eba8469b88f212f1012c357`);
+
+      expect(res.statusCode).toBe(404);
+    });
   });
 });
