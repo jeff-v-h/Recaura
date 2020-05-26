dd33bba94a256c2ea7cec84ce611ebca8da5fc50
jeff-h <jeffvh@outlook.com>
2020-05-26 11:16:52 +1000

    add token to api service requests

--- client/src/services/apiService.ts
+++ client/src/services/apiService.ts
@@ -11,30 +11,42 @@ export class ApiService {
     }
   };
 
-  get(url: string, showDefaultErrorMsg: boolean = true): Promise<AxiosResponse | AxiosError> {
-    return axios.get(url, ApiService.AxiosConfig).catch(handleError(showDefaultErrorMsg));
+  get(
+    url: string,
+    token?: string,
+    showDefaultErrorMsg: boolean = true
+  ): Promise<AxiosResponse | AxiosError> {
+    const config = getConfig(token);
+    return axios.get(url, config).catch(handleError(showDefaultErrorMsg));
   }
 
   post(
     url: string,
     payload: any,
+    token?: string,
     showDefaultErrorMsg: boolean = true
   ): Promise<AxiosResponse | AxiosError> {
-    return axios.post(url, payload, ApiService.AxiosConfig).catch(handleError(showDefaultErrorMsg));
+    const config = getConfig(token);
+    return axios.post(url, payload, config).catch(handleError(showDefaultErrorMsg));
   }
 
   patch(
     url: string,
     payload: any,
+    token?: string,
     showDefaultErrorMsg: boolean = true
   ): Promise<AxiosResponse | AxiosError> {
-    return axios
-      .patch(url, payload, ApiService.AxiosConfig)
-      .catch(handleError(showDefaultErrorMsg));
+    const config = getConfig(token);
+    return axios.patch(url, payload, config).catch(handleError(showDefaultErrorMsg));
   }
 
-  delete(url: string, showDefaultErrorMsg: boolean = true): Promise<AxiosResponse | AxiosError> {
-    return axios.delete(url, ApiService.AxiosConfig).catch(handleError(showDefaultErrorMsg));
+  delete(
+    url: string,
+    token?: string,
+    showDefaultErrorMsg: boolean = true
+  ): Promise<AxiosResponse | AxiosError> {
+    const config = getConfig(token);
+    return axios.delete(url, config).catch(handleError(showDefaultErrorMsg));
   }
 
   handleRequestError = (e: string) => {
@@ -42,6 +54,12 @@ export class ApiService {
     return Promise.reject(e);
   };
 }
+
+const getConfig = (token?: string) => {
+  const config = { ...ApiService.AxiosConfig };
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+};
 
 const handleError = (showDefaultErrorMsg: boolean) => (e: AxiosError) => {
   if (showDefaultErrorMsg) {
